17ce3ca6ccbab582ba194321c89984ba7583f781
Denis-lab-911 <Denis-lab-911@users.noreply.github.com>
2025-11-27 15:28:49 +0000

    deploy: d66640120f28ac85ed0748a5d785038dd03c2458

--- tests/hello.test.js
+++ tests/hello.test.js
@@ -1,22 +1,18 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
 describe("Vérification du titre <h1> dans index.html", () => {
   test("le titre doit être 'Hello World depuis GitHub Codespaces !'", () => {
-    // Charger le contenu du HTML
     const filePath = path.resolve(__dirname, "../index.html");
     const html = fs.readFileSync(filePath, "utf8");
 
-    // Injecter le HTML dans JSDOM
     document.body.innerHTML = html;
 
-    // Récupérer le <h1>
     const h1 = document.querySelector("h1");
 
-    // Vérifier qu'il existe
     expect(h1).not.toBeNull();
-
-    // Vérifier le texte exact
-    expect(h1.textContent.trim()).toBe("Hello World depuis GitHub Codespaces !");
+    expect(h1.textContent.trim()).toBe(
+      "Hello World depuis GitHub Codespaces !"
+    );
   });
 });
